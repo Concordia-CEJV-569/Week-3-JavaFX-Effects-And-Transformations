50108da0a8306050c64f4c5749875c79902fb32d
Pranav Bhatia <bhatiapranav1996@gmail.com>
2021-09-27 22:28:59 -0400

    Rotate a Button

--- src/main/java/com/example/javafxeffectsandtransformations/HelloApplication.java
+++ src/main/java/com/example/javafxeffectsandtransformations/HelloApplication.java
@@ -1,23 +1,75 @@
 package com.example.javafxeffectsandtransformations;
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application implements EventHandler {
+    private Button rotateButton;
+    private Button blurButton;
+    private Button scaleButton;
+    private Rotate rotate;
+    private double angle;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Life Cycle");
+
+        angle = 0.0;
+
+        rotateButton = new Button("Rotate");
+        blurButton = new Button("Blur");
+        scaleButton = new Button("Scale");
+
+        rotate = new Rotate();
+
+        // Register our buttons
+        rotateButton.setOnAction(this);
+        blurButton.setOnAction(this);
+        scaleButton.setOnAction(this);
+
+        Label reflectLabel = new Label("Reflection Adds Visual Sparkle");
+
+        FlowPane rootFlowPane = new FlowPane(15, 15);
+        rootFlowPane.setAlignment(Pos.CENTER);
+
+        rootFlowPane.getChildren().addAll(rotateButton, blurButton, scaleButton, reflectLabel);
+
+        Scene scene = new Scene(rootFlowPane, 300, 120);
         stage.setScene(scene);
         stage.show();
+
+        // Setup transformations
+        rotateButton.getTransforms().add(rotate);
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void handle(Event event) {
+        if (event.getSource().equals(rotateButton)) {
+            angle += 15.0;
+            rotate.setAngle(angle);
+            rotate.setPivotX(rotateButton.getWidth() / 2);
+            rotate.setPivotY(rotateButton.getHeight() / 2);
+            rotate.setPivotZ(rotateButton.getHeight() / 2);
+        } else if (event.getSource().equals(blurButton)) {
+            System.out.println("Blur button clicked");
+        } else if (event.getSource().equals(scaleButton)) {
+            System.out.println("Scale button clicked");
+        }
     }
 }
